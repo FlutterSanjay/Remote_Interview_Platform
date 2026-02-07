621931d4ec87002e08281e4f18cf9103d0cf720c
FlutterSanjay <sanjayshaw80138@gmail.com>
2026-02-07 12:17:46 +0530

    SetUp DB , Clerk & GetStream

---
+++ backend/src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { ENV } from "../helper/env.js";
+
+const connectDB = async () => {
+  try {
+    if (!ENV.DB_URL) {
+      throw new Error("DB_URL is not define in Env");
+    }
+    const conn = await mongoose.connect(ENV.DB_URL);
+    console.log("Connected To MongoDB:", conn.connection.host);
+  } catch (err) {
+    console.error("Error connecting to MongoDB", error);
+    process.exit(1); // 0 means success , 1 means fails
+  }
+};
+
+export default connectDB;
